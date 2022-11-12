b37ff7277bdaf13a7b8a09f0157364e0fe412b09
LukaszWilk1 <107866108+LukaszWilk1@users.noreply.github.com>
2022-11-12 19:15:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -24,14 +24,44 @@ let pictures = [
   "card-eight",
   "card-eight"
 ];
-const shuffle = arg => {
+let cards = [
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+  {card: undefined, value:undefined},
+];
+/*const shuffle = arg => {
   index = Math.floor(Math.random() * pictures.length);                          //getting random number and asigning it to variable index
   arg.setAttribute("value", pictures[index]);                                   //setting value attribute of actual td element to one of classes from pictures list
   pictures.splice(index, 1);                                                    //removing already used element from pictures list
+}*/
+const shuffle = () => {
+  index = Math.floor(Math.random() * pictures.length);
+  let value = pictures[index];
+  pictures.splice(index, 1);
+  return value;
+}
+const reverse = card => {
+  card.classList.add(cards[cards.map(object => object.card).indexOf(card)].value);
 }
 for (let i = 0; i < 16; i++) {
-  shuffle(document.getElementsByTagName("td")[i]);                              //callback to shuffle function
+  cards[i].value = shuffle();                              //callback to shuffle function
+  cards[i].card=document.getElementsByTagName("td")[i];
   document.getElementsByTagName("td")[i].addEventListener("click", (e) => {
     e.target.classList.add(e.target.getAttribute("value"));                     //showing card on click by passing value element to class
+    reverse(e.target);
   })
 }
+console.log(cards);
