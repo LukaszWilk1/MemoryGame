3abb284e338ebfbdfb02a5be6fab76acf2dafd85
LukaszWilk1 <107866108+LukaszWilk1@users.noreply.github.com>
2022-11-15 21:12:50 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -47,6 +47,33 @@ let cards = [
   arg.setAttribute("value", pictures[index]);                                   //setting value attribute of actual td element to one of classes from pictures list
   pictures.splice(index, 1);                                                    //removing already used element from pictures list
 }*/
+
+const game = e => {
+  turnedCards++;
+  if(turnedCards==2){
+    for(let i=0; i<cards.length; i++){
+      cards[i].card.classList.add("unclicable");
+    }
+    setTimeout(()=>{
+      check();
+    }, 750);
+  }
+  reverse(e.target);
+}
+
+const start = e => {
+  console.log(pictures);
+  document.getElementById("start").innerText="Start";
+  for (let i = 0; i < 16; i++) {
+    cards[i].value = shuffle();                              //callback to shuffle function
+    cards[i].card=document.getElementsByTagName("td")[i];
+    document.getElementsByTagName("td")[i].addEventListener("click", game);
+    document.getElementById("start").style="visibility: hidden";
+    document.getElementById("start").removeEventListener("click", start);
+  }
+}
+document.getElementById("start").addEventListener("click", start);
+
 const shuffle = () => {
   index = Math.floor(Math.random() * pictures.length);
   let value = pictures[index];
@@ -60,6 +87,44 @@ const reverse = card => {
   else if(turnedCards==2) secondCard = cards[cards.map(object => object.card).indexOf(card)].value;
   console.log(firstCard, secondCard);
 }
+
+const reset = e => {
+  turnedCards = 0;
+  firstCard;
+  secondCard;
+  seconds = 00;
+  minutes = 00;
+  pairs = 0;
+  index = 0;
+  pictures = [
+    "card-one",
+    "card-one",
+    "card-two",
+    "card-two",
+    "card-three",
+    "card-three",
+    "card-four",
+    "card-four",
+    "card-five",
+    "card-five",
+    "card-six",
+    "card-six",
+    "card-seven",
+    "card-seven",
+    "card-eight",
+    "card-eight"
+  ];
+  for(let i=0;i<16;i++){
+    cards[i].value = undefined;
+    cards[i].card.classList="";
+    cards[i].card=undefined;
+    document.getElementsByTagName("td")[i].removeEventListener("click", game);
+  }
+  document.getElementById("start").innerText="Start";
+  document.getElementById("start").removeEventListener("click", reset)
+  document.getElementById("start").addEventListener("click", start);
+}
+
 const check = () => {
   if(turnedCards==2){
     for(let i=0; i<cards.length; i++){
@@ -72,8 +137,10 @@ const check = () => {
         ||cards[i].card.classList.contains("card-seven")||cards[i].card.classList.contains("card-eight")){
           cards[i].card.classList="";
           cards[i].card.classList.add("winner");
+          cards[i].card.classList.add("unclicable");
         }
       }
+      pairs++;
     }
     else{
       for(let i=0; i<cards.length; i++){
@@ -87,21 +154,12 @@ const check = () => {
     }
   }
   turnedCards=0;
+  if(pairs===8){
+    document.getElementById("start").innerText="Restart";
+    document.getElementById("start").style="visibility: visible";
+    document.getElementById("start").addEventListener("click", reset);
+    console.log(cards);
+  }
 }
-for (let i = 0; i < 16; i++) {
-  cards[i].value = shuffle();                              //callback to shuffle function
-  cards[i].card=document.getElementsByTagName("td")[i];
-  document.getElementsByTagName("td")[i].addEventListener("click", (e) => {
-    turnedCards++;
-    if(turnedCards==2){
-      for(let i=0; i<cards.length; i++){
-        cards[i].card.classList.add("unclicable");
-      }
-      setTimeout(()=>{
-        check();
-      }, 1000);
-    }
-    reverse(e.target);
-  })
-}
+
 console.log(cards);
